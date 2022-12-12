e59a2eab20b20b5ef4c62c0ca1c8967a0392dee3
Giuliana Squillacioti <giulisquillacioti@gmail.com>
2022-12-12 11:34:07 -0300

    href

--- js/script.js
+++ js/script.js
@@ -13,12 +13,17 @@ formBtn.addEventListener('click', () => {
     if (name.value, phone.value, email.value, message.value) {
         if (validateEmail(email.value)) {
             console.log(`${name.value}, ${phone.value}, ${email.value}, ${message.value}`)
+            Swal.fire({
+                title: `¡Gracias por comunicarte con nosotros!
+                En los próximos días estarás recibiendo una respuesta.`,
+                confirmButtonColor: 'rgb(83, 61, 24)'
+            })
         } else {
             Swal.fire({
                 title: `La dirección de correo electrónico no es válida.`,
                 confirmButtonColor: 'rgb(83, 61, 24)'
             })
-        } 
+        }
     } else {
         Swal.fire({
             title: `Todos los campos deben ser completados para enviar el formulario.`,
